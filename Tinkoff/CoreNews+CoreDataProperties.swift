5e16dce927fc0078cc4228c5f530cce8dbeaba86
Mr Ocumare <ilyaocumare@gmail.com>
2019-09-26 05:41:21 +0300

    add entity

---
+++ Tinkoff/CoreNews+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  CoreNews+CoreDataProperties.swift
+//  Tinkoff
+//
+//  Created by Mr.Ocumare on 26/09/2019.
+//  Copyright © 2019 Ilya Ocumare. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CoreNews {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CoreNews> {
+        return NSFetchRequest<CoreNews>(entityName: "CoreNews")
+    }
+
+    @NSManaged public var viewCount: Int16
+    @NSManaged public var slug: String?
+    @NSManaged public var tittle: String?
+    @NSManaged public var text: String?
+    @NSManaged public var date: String?
+    @NSManaged public var id: String?
+    @NSManaged public var user: User?
+
+}
